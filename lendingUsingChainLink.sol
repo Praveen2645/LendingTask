ae13a498ce85db4f9767c2c3fa4eeb7d1420f198
Praveen Patawkar <107745548+Praveen2645@users.noreply.github.com>
2023-06-06 08:43:09 +0530

    Update lendingUsingChainLink.sol

--- lendingUsingChainLink.sol
+++ lendingUsingChainLink.sol
@@ -7,11 +7,8 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-
 interface Token {
-    function transfer(address recipient, uint256 amount)
-        external
-        returns (bool);
+    function transfer(address recipient, uint256 amount) external returns (bool);
 
     function balanceOf(address account) external view returns (uint256);
 
@@ -24,21 +21,20 @@ interface Token {
 
 contract LendingAndBorrowing is Ownable, ReentrancyGuard, Pausable {
     AggregatorV3Interface internal priceFeed;
-
     Token PBMCToken;
-
     uint256 interestRate;
 
-   constructor(Token _tokenAddress, address _priceFeedAddress) {
+    constructor(Token _tokenAddress, address _priceFeedAddress) {
+        require(
+            address(_tokenAddress) != address(0),
+            "Token Address cannot be address 0"
+        );
+        interestRate = 10;
+        PBMCToken = _tokenAddress;
 
-    require(address(_tokenAddress) != address(0), "Token Address cannot be address 0");
-    interestRate = 10;
-    PBMCToken = _tokenAddress;
-
-    // Initialize the Chainlink price feed
-    priceFeed = AggregatorV3Interface(_priceFeedAddress);
-}
-
+        // Initialize the Chainlink price feed
+        priceFeed = AggregatorV3Interface(_priceFeedAddress);
+    }
 
     address[] public lenders;
     address[] public borrowers;
@@ -76,7 +72,7 @@ contract LendingAndBorrowing is Ownable, ReentrancyGuard, Pausable {
 
         // Update the lender's balance
         _tokenBalances[msg.sender] += pbmcAmount;
-         uint256 interestEarned = (msg.value * interestRate) / 100;
+        uint256 interestEarned = (msg.value * interestRate) / 100;
 
         _lenders[msg.sender] = Lenders(
             msg.sender,
@@ -108,7 +104,7 @@ contract LendingAndBorrowing is Ownable, ReentrancyGuard, Pausable {
 
         // Update the borrower's balance
         _tokenBalances[msg.sender] += etherAmount;
-       
+
         // Store the borrower's details
         _borrowers[msg.sender] = Borrowers(
             msg.sender,
@@ -119,18 +115,16 @@ contract LendingAndBorrowing is Ownable, ReentrancyGuard, Pausable {
         borrowers.push(msg.sender);
     }
 
-    function repay() external payable {
+    function repay(uint256 _amount) external payable {
         // Check if the borrower has an active loan
         Borrowers storage borrower = _borrowers[msg.sender];
         require(
-            borrower.PBMCdeposited> 0 && borrower.borrower == msg.sender,
+            borrower.PBMCdeposited > 0 && borrower.borrower == msg.sender,
             "No active loan found"
         );
 
-        uint256 repaymentAmount = msg.value;
-
-        // Calculate the equivalent amount of PBMC tokens to return
-        uint256 pbmcAmount = calculatePBMC(repaymentAmount);
+        // Calculate the equivalent amount of PBMC tokens to return based on the amount being repaid
+        uint256 pbmcAmount = calculatePBMC(_amount);
 
         // Check if the contract has enough PBMC tokens to return
         require(
@@ -141,53 +135,63 @@ contract LendingAndBorrowing is Ownable, ReentrancyGuard, Pausable {
         // Transfer the equivalent PBMC tokens from the contract to the borrower
         require(PBMCToken.transfer(msg.sender, pbmcAmount), "Transfer failed");
 
-        // Clear the borrower's details
-        delete _borrowers[msg.sender];
+        // Update the borrower's balance and amount remaining
+        _tokenBalances[msg.sender] -= _amount;
+        borrower.PBMCdeposited -= pbmcAmount;
 
         // Transfer any remaining excess ether back to the borrower
-        if (msg.value > repaymentAmount) {
-            uint256 excessAmount = msg.value - repaymentAmount;
+        if (msg.value > _amount) {
+            uint256 excessAmount = msg.value - _amount;
             payable(msg.sender).transfer(excessAmount);
         }
     }
 
-    function calculatePBMC(uint256 etherAmount) private pure returns (uint256) {
-        uint256 exchangeRate = 2; // Assume the exchange rate is 10 PBMC per wei
-        return etherAmount * exchangeRate;
+    function withdraw() external payable {
+        Lenders storage lender = _lenders[msg.sender];
+        require(lender.lender == msg.sender, "No lending found");
+        require(lender.lendedAmount > 0, "No amount to withdraw");
+
+        uint256 amountToWithdraw = lender.lendedAmount + lender.interestEarned;
+
+        // Transfer the deposited ethers + interest to the lender
+        payable(msg.sender).transfer(amountToWithdraw);
+
+        // Transfer the PBMC tokens owned in exchange back to the contract owner
+        require(
+            PBMCToken.transferFrom(
+                msg.sender,
+                owner(),
+                lender.PBMCinReturn
+            ),
+            "PBMC transfer failed"
+        );
+
+        // Reset the lender's details
+        delete _lenders[msg.sender];
+    }
+
+    function calculatePBMC(uint256 etherAmount) private view returns (uint256) {
+        uint256 etherPriceInUSD = getCurrentEtherPrice();
+        uint256 pbmcPriceInUSD = 5;
+        uint256 pbmcAmount = (etherAmount * etherPriceInUSD) / pbmcPriceInUSD;
+        return pbmcAmount;
     }
 
     function calculateEtherAmount(uint256 pbmcAmount)
         private
-        pure
+        view
         returns (uint256)
     {
-        //uint256 exchangeRate = 1; // 2 PBMC will return 1 wei
-        return pbmcAmount / 2;
+        uint256 etherPriceInUSD = getCurrentEtherPrice();
+        uint256 pbmcPriceInUSD = 5;
+        uint256 etherAmount = (pbmcAmount * pbmcPriceInUSD) / etherPriceInUSD;
+        return etherAmount;
     }
 
-     function withdraw() external payable{
-    Lenders storage lender = _lenders[msg.sender];
-    require(lender.lender == msg.sender, "No lending found");
-    require(lender.lendedAmount > 0, "No amount to withdraw");
-
-    uint256 amountToWithdraw = lender.lendedAmount + lender.interestEarned;
-
-    // Transfer the deposited ethers + interest to the lender
-    payable(msg.sender).transfer(amountToWithdraw);
-
-    // Transfer the PBMC tokens owned in exchange back to the contract owner
-    require(
-        PBMCToken.transferFrom(msg.sender, owner(), lender.PBMCinReturn),
-        "PBMC transfer failed"
-    );
-
-    // Reset the lender's details
-    delete _lenders[msg.sender];
-}
-function getCurrentEtherPrice() public view returns (uint256) {
-    (, int256 price, , , ) = priceFeed.latestRoundData();
-    return uint256(price);
-}
+    function getCurrentEtherPrice() public view returns (uint256) {
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        return uint256(price);
+    }
 
     function pause() public onlyOwner {
         _pause();
